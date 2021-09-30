134611bf129fbb58ccccfb5fb17d97b7af8f052e
Bladimir Lora <bladimir.lora.solis@gmail.com>
2021-09-30 20:49:27 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WiredBrainCoffe.StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new SimpleStack();
+            stack.Push(1.8);
+            stack.Push(5.4);
+            stack.Push(3.9);
+        }
+    }
+}
---
+++ SimpleStack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WiredBrainCoffe.StackApp
+{
+    internal class SimpleStack
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+        public SimpleStack()
+        {
+            _items = new double[10];
+        }
+
+        public void Push(double item)
+        {
+            _currentIndex++;
+            _items[_currentIndex] = item;
+        }
+    }
+}
